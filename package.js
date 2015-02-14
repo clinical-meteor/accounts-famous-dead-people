becce76927330ce1476c5f898d4816ab0c066a37
Adam Renfro <adamrenfro@Adams-Mac-mini-3.local>
2015-02-13 19:24:57 -0500

    error in version; bump

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   summary: "Adds famous dead people to the Meteor.users collection.",
 
   // update this value before you run 'meteor publish'
-  version: "1.0.0",
+  version: "1.0.1",
 
   // if this value isn't set, meteor will default to the directory name
   name: "clinical:accounts-famous-dead-people",
