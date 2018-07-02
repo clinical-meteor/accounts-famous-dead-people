fcae927f203bd5b38f7bacde860653527415101c
Abigail Watson <abigailwatson@Abigails-Mac-mini-317772.local>
2018-07-02 00:03:03 -0500

    tests

---
+++ tests/FamousDeadPeople.tests.js
@@ -0,0 +1,16 @@
+import { Meteor } from 'meteor/meteor';
+import { assert } from 'meteor/practicalmeteor:chai';
+import { chai } from 'meteor/practicalmeteor:chai';
+import { expect } from 'meteor/practicalmeteor:chai';
+
+describe('clinical:hl7-resource-allergy-intolerance', function () {
+  beforeEach(function () {
+    //console.log('beforeEach');
+  });
+  afterEach(function () {
+    //console.log('afterEach');
+  });
+  it('exists globally', function () {
+    expect(Characters).to.exist;
+  });
+});
